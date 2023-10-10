9e5e7e4a436fbd2ebce1715e6a429387f80146a7
Andrés Alvarez <andres.alvarez@andresalvarez-MacBook-Pro-7R6H.local>
2023-10-10 22:19:42 +0200

    update port

--- model/database.js
+++ model/database.js
@@ -6,17 +6,9 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
-  user: DB_USER || "root",
-  password: DB_PASS,
-  database: DB_NAME || "todos",
-  multipleStatements: true,
-  port: DB_PORT || 3306
-});
-
-console.log("debug", {
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
